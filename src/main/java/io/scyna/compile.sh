2089017a10349d52c542f8b57f25da3db08ca30f
NGUYEN Hong San <hongsan@gmail.com>
2022-04-17 18:37:50 +0700

    protobuf

--- src/main/java/io/scyna/compile.sh
+++
@@ -1,2 +0,0 @@
-protoc --java_out=../../ velo.proto
-protoc --java_out=../../ manager.proto
---
+++ src/proto/compile.sh
@@ -0,0 +1,3 @@
+protoc --java_out=../ scyna.proto
+protoc --java_out=../ error.proto
+protoc --java_out=../ engine.proto
