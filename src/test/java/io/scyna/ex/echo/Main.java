a45611e1613970acb96891da0eb38ccf875d9c42
NGUYEN Hong San <hongsan@gmail.com>
2022-04-20 21:33:41 +0700

    echo worked

--- src/test/java/io/scyna/ex/echo/Main.java
+++ src/test/java/io/scyna/ex/echo/Main.java
@@ -7,7 +7,7 @@ import io.scyna.ex.echo.proto.EchoResponse;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Engine.init("http://127.0.0.1:8081", "Test", "123456");
+        Engine.init("http://127.0.0.1:8081", "scyna.test", "123456");
         Service.register("/test/echo", new EchoService());
 
         var request = EchoRequest.newBuilder().setText("hello").build();
