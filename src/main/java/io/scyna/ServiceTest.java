20b9f1271bf96449f6c50ffd9ce786374a63d3e6
NGUYEN Hong San <hongsan@gmail.com>
2022-05-02 09:21:26 +0700

    ServiceTest

---
+++ src/main/java/io/scyna/ServiceTest.java
@@ -0,0 +1,69 @@
+package io.scyna;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.google.protobuf.Message;
+import com.google.protobuf.Parser;
+
+public class ServiceTest {
+    private String url;
+    private Message request;
+    private Message response;
+    private int status;
+
+    private ServiceTest(String url) {
+        this.url = url;
+    }
+
+    public static ServiceTest New(String url) {
+        return new ServiceTest(url);
+    }
+
+    public ServiceTest withRequest(Message request) {
+        this.request = request;
+        return this;
+    }
+
+    public ServiceTest expectSuccess() {
+        this.status = 200;
+        return this;
+    }
+
+    public ServiceTest expectError(io.scyna.proto.Error error) {
+        this.status = 400;
+        this.response = error;
+        return this;
+    }
+
+    public ServiceTest expectResponse(Message response) {
+        this.status = 200;
+        this.response = response;
+        return this;
+    }
+
+    public void run() {
+        var res = Service.sendRequest(url, request);
+        assertNotNull(res);
+        assertEquals(status, res.getCode());
+
+        if (response != null) {
+            var r1 = res.getBody();
+            var r2 = response.toByteString();
+            assertEquals(r1, r2); // TODO: verify
+        }
+    }
+
+    public <T extends Message> T run(Parser<T> parser) {
+        var res = Service.sendRequest(url, request);
+        assertNotNull(res);
+        assertEquals(status, res.getCode());
+        try {
+            return parser.parseFrom(res.getBody());
+        } catch (InvalidProtocolBufferException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
--- src/main/java/io/scyna/testing/Service.java
+++
@@ -1,5 +0,0 @@
-package io.scyna.testing;
-
-public class Service {
-
-}
