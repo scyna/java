7b1fb1fa55f43992de4d2263b3b4aa5db700a4f3
NGUYEN Hong San <hongsan@gmail.com>
2022-06-04 13:21:14 +0700

    minor update

--- src/main/java/io/scyna/ActivityStream.java
+++ src/main/java/io/scyna/ActivityStream.java
@@ -9,28 +9,25 @@ public class ActivityStream {
     private final static int TRY_COUNT = 10;
     private String tableName;
 
-    public ActivityStream(String keyspace) {
-        this.tableName = keyspace + ".activity";
+    public ActivityStream(String table) {
+        this.tableName = table;
     }
 
     public void add(long entity, int type, Message activity) {
         var session = Engine.DB().session();
         try {
-            var buf = activity.toByteArray();
-            var buffer = ByteBuffer.wrap(buf);
+            var data = activity.toByteArray();
+            var buffer = ByteBuffer.wrap(data);
             var time = Utils.currentMicroSeconds();
             var qInsert = session.prepare(
                     "INSERT INTO " + tableName + " (entity_id, type, time, data) VALUES(?,?,?,?) IF NOT EXISTS");
             var statement = new BoundStatement(qInsert);
 
-            int count = 0;
-            while (count < TRY_COUNT) {
-                var rs = session.execute(statement.bind(entity, type, time, buffer));
+            for (int i = 0; i < TRY_COUNT; i++) {
+                var rs = session.execute(statement.bind(entity, type, time + i, buffer));
                 if (rs.one().getBool("applied")) {
                     break;
                 }
-                time++;
-                count++;
             }
         } catch (Exception e) {
             e.printStackTrace();
