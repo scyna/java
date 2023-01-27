19f46f4c0292fd572d1b63b21331360352a88a89
NGUYEN Hong San <hongsan@gmail.com>
2023-01-27 12:28:05 +0700

    working on Value Object

--- src/test/java/io/scyna/ex/customer/model/Customer.java
+++ src/test/java/io/scyna/ex/customer/model/Customer.java
@@ -1,5 +1,11 @@
 package io.scyna.ex.customer.model;
 
 public class Customer {
+    public Identity identity;
+    public String name;
+    public EmailAddress email;
+    public PhoneNumber phone;
+    public Gender gender;
+    public StreetAddress address;
 
 }
--- src/test/java/io/scyna/ex/customer/model/EmailAddress.java
+++ src/test/java/io/scyna/ex/customer/model/EmailAddress.java
@@ -1,5 +1,18 @@
 package io.scyna.ex.customer.model;
 
 public class EmailAddress {
+    String value;
 
+    public static EmailAddress parse(String email) {
+        /* TODO: validation */
+        return new EmailAddress(email);
+    }
+
+    private EmailAddress(String email) {
+        this.value = email;
+    }
+
+    public String ToString() {
+        return value;
+    }
 }
