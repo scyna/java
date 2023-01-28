d394eff60d639c9abfbc497d95376ba7a1f7938c
NGUYEN Hong San <hongsan@gmail.com>
2023-01-28 22:28:53 +0700

    test identity

---
+++ src/test/java/io/scyna/ex/customer/test/IdentityTest.java
@@ -0,0 +1,47 @@
+package io.scyna.ex.customer.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import io.scyna.ex.customer.model.Error;
+import io.scyna.ex.customer.model.Identity;
+
+public class IdentityTest {
+    @Test
+    public void testCreateIdentity() {
+        try {
+            var identity = Identity.create("CMND", "123456789");
+            assertEquals(identity.getType(), "CMND");
+            assertEquals(identity.getNumber(), "123456789");
+            assertEquals(identity.toString(), "CMND:123456789");
+        } catch (io.scyna.Error e) {
+            assertTrue(false);
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testParseIdentity() {
+        try {
+            var identity = Identity.parse("CCCD:123456789");
+            assertEquals(identity.getType(), "CCCD");
+            assertEquals(identity.getNumber(), "123456789");
+            assertEquals(identity.toString(), "CCCD:123456789");
+        } catch (io.scyna.Error e) {
+            assertTrue(false);
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testParseIdentityFail() {
+        try {
+            Identity.parse("CCCD_123456789");
+        } catch (io.scyna.Error e) {
+            assertEquals(e, Error.IDENTITY_INVALID);
+            e.printStackTrace();
+        }
+    }
+}
