90772014773fa7c7204d37637c92c2c16701b204
NGUYEN Hong San <hongsan@gmail.com>
2023-01-29 11:09:10 +0700

    minor update

--- src/test/java/io/scyna/ex/customer/service/CreateCustomerService.java
+++ src/test/java/io/scyna/ex/customer/service/CreateCustomerService.java
@@ -9,8 +9,6 @@ import io.scyna.ex.customer.proto.*;
 public class CreateCustomerService extends Endpoint.Handler<CreateCustomerRequest> {
     @Override
     public void execute() throws io.scyna.Error {
-        context.info("Receive CreateCustmerRequest");
-
         var repository = CustomerService.loadRepository(context);
 
         var customer = new Customer();
@@ -18,12 +16,8 @@ public class CreateCustomerService extends Endpoint.Handler<CreateCustomerReques
         customer.name = request.getName();
         customer.identity = Identity.create(request.getIDType(), request.getIDNumber());
 
-        context.info("ok 1");
-
         CustomerService.assureIdentityNotExists(repository, customer.identity);
-        context.info("ok 2");
         repository.createCustomer(customer);
-        context.info("ok 3");
         response(CreateCustomerResponse.newBuilder().setID(customer.ID).build());
     }
 }
