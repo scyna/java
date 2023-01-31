ba564251ecaea01d2b7390b47b328f968b4aa4f1
NGUYEN Hong San <hongsan@gmail.com>
2023-01-31 09:00:19 +0700

    minor update

--- src/test/java/io/scyna/ex/account/model/Error.java
+++ src/test/java/io/scyna/ex/account/model/Error.java
@@ -2,6 +2,6 @@ package io.scyna.ex.account.model;
 
 public class Error {
         public static final io.scyna.Error ACCOUNT_EXISTS = io.scyna.Error.New(100, "Account Exists");
-        public static final io.scyna.Error EMAIL_INVALID = io.scyna.Error.New(100, "Email Invalid");
-        public static final io.scyna.Error ACCOUNT_NOT_FOUND = io.scyna.Error.New(101, "Account Not Found");
+        public static final io.scyna.Error EMAIL_INVALID = io.scyna.Error.New(101, "Email Invalid");
+        public static final io.scyna.Error ACCOUNT_NOT_FOUND = io.scyna.Error.New(102, "Account Not Found");
 }
