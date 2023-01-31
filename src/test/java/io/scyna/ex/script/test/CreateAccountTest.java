b02b99ce40c790f910c8fdd642c4536018b184c1
NGUYEN Hong San <hongsan@gmail.com>
2023-01-31 20:50:32 +0700

    formatter

--- src/test/java/io/scyna/ex/script/test/CreateAccountTest.java
+++ src/test/java/io/scyna/ex/script/test/CreateAccountTest.java
@@ -16,70 +16,70 @@ import io.scyna.ex.script.service.Path;
 
 public class CreateAccountTest {
 
-        @BeforeClass
-        public static void setup() throws Exception {
-                Engine.init("http://127.0.0.1:8081", "scyna.test", "123456");
-                Endpoint.register(Path.CREATE_ACCOUNT_URL, new CreateAccountService());
-        }
+    @BeforeClass
+    public static void setup() throws Exception {
+        Engine.init("http://127.0.0.1:8081", "scyna.test", "123456");
+        Endpoint.register(Path.CREATE_ACCOUNT_URL, new CreateAccountService());
+    }
 
-        @AfterClass
-        public static void teardown() {
-                cleanup();
-                Engine.release();
-        }
+    @AfterClass
+    public static void teardown() {
+        cleanup();
+        Engine.release();
+    }
 
-        @Test
-        public void testCreateCustomer() {
-                EndpointTest.New(Path.CREATE_ACCOUNT_URL)
-                                .withRequest(CreateAccountRequest.newBuilder()
-                                                .setEmail("a@gmail.com")
-                                                .setName("Nguyen Van A")
-                                                .setPassword("12345678")
-                                                .build())
-                                .expectSuccess()
-                                .run();
+    @Test
+    public void testCreateCustomer() {
+        EndpointTest.New(Path.CREATE_ACCOUNT_URL)
+                .withRequest(CreateAccountRequest.newBuilder()
+                        .setEmail("a@gmail.com")
+                        .setName("Nguyen Van A")
+                        .setPassword("12345678")
+                        .build())
+                .expectSuccess()
+                .run();
 
-                EndpointTest.New(Path.CREATE_ACCOUNT_URL)
-                                .withRequest(CreateAccountRequest.newBuilder()
-                                                .setEmail("a@gmail.com")
-                                                .setName("Nguyen Van A")
-                                                .setPassword("12345678")
-                                                .build())
-                                .expectError(Error.ACCOUNT_EXISTS)
-                                .run();
-        }
+        EndpointTest.New(Path.CREATE_ACCOUNT_URL)
+                .withRequest(CreateAccountRequest.newBuilder()
+                        .setEmail("a@gmail.com")
+                        .setName("Nguyen Van A")
+                        .setPassword("12345678")
+                        .build())
+                .expectError(Error.ACCOUNT_EXISTS)
+                .run();
+    }
 
-        @Test
-        public void testCreateCustomerBadEmail() {
-                EndpointTest.New(Path.CREATE_ACCOUNT_URL)
-                                .withRequest(CreateAccountRequest.newBuilder()
-                                                .setEmail("a+gmail.com")
-                                                .setName("Nguyen Van A")
-                                                .setPassword("12345678")
-                                                .build())
-                                .expectError(io.scyna.Error.REQUEST_INVALID)
-                                .run();
+    @Test
+    public void testCreateCustomerBadEmail() {
+        EndpointTest.New(Path.CREATE_ACCOUNT_URL)
+                .withRequest(CreateAccountRequest.newBuilder()
+                        .setEmail("a+gmail.com")
+                        .setName("Nguyen Van A")
+                        .setPassword("12345678")
+                        .build())
+                .expectError(io.scyna.Error.REQUEST_INVALID)
+                .run();
 
-                EndpointTest.New(Path.CREATE_ACCOUNT_URL)
-                                .withRequest(CreateAccountRequest.newBuilder()
-                                                .setName("Nguyen Van A")
-                                                .setPassword("12345678")
-                                                .build())
-                                .expectError(io.scyna.Error.REQUEST_INVALID)
-                                .run();
+        EndpointTest.New(Path.CREATE_ACCOUNT_URL)
+                .withRequest(CreateAccountRequest.newBuilder()
+                        .setName("Nguyen Van A")
+                        .setPassword("12345678")
+                        .build())
+                .expectError(io.scyna.Error.REQUEST_INVALID)
+                .run();
 
-                EndpointTest.New(Path.CREATE_ACCOUNT_URL)
-                                .withRequest(CreateAccountRequest.newBuilder()
-                                                .setEmail("")
-                                                .setName("Nguyen Van A")
-                                                .setPassword("12345678")
-                                                .build())
-                                .expectError(io.scyna.Error.REQUEST_INVALID)
-                                .run();
-        }
+        EndpointTest.New(Path.CREATE_ACCOUNT_URL)
+                .withRequest(CreateAccountRequest.newBuilder()
+                        .setEmail("")
+                        .setName("Nguyen Van A")
+                        .setPassword("12345678")
+                        .build())
+                .expectError(io.scyna.Error.REQUEST_INVALID)
+                .run();
+    }
 
-        private static void cleanup() {
-                var truncate = QueryBuilder.truncate("ex_account", "account");
-                Engine.DB().session().execute(truncate);
-        }
+    private static void cleanup() {
+        var truncate = QueryBuilder.truncate("ex_account", "account");
+        Engine.DB().session().execute(truncate);
+    }
 }
