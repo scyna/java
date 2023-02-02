ced4896def45593f67d2dc859e13b9ac4c9699c2
NGUYEN Hong San <hongsan@gmail.com>
2023-02-02 16:14:48 +0700

    minor update

--- src/test/java/io/scyna/ex/account/build/Reader.java
+++ src/test/java/io/scyna/ex/account/build/Reader.java
@@ -2,6 +2,7 @@ package io.scyna.ex.account.build;
 
 import io.scyna.Endpoint;
 import io.scyna.Engine;
+import io.scyna.Event;
 import io.scyna.ex.account.service.GetAccountByEmailService;
 import io.scyna.ex.account.service.Path;
 
@@ -9,5 +10,6 @@ public class Reader {
     public static void main(String[] args) throws Exception {
         Engine.init("http://127.0.0.1:8081", "ex_account", "123456");
         Endpoint.register(Path.GET_ACCOUNT_BY_EMAIl_URL, new GetAccountByEmailService());
+        Event.startListening();
     }
 }
--- src/test/java/io/scyna/ex/account/build/Writer.java
+++ src/test/java/io/scyna/ex/account/build/Writer.java
@@ -3,7 +3,6 @@ package io.scyna.ex.account.build;
 import io.scyna.Command;
 import io.scyna.Endpoint;
 import io.scyna.Engine;
-import io.scyna.Event;
 import io.scyna.ex.account.service.CreateAccountService;
 import io.scyna.ex.account.service.Path;
 
@@ -12,6 +11,5 @@ public class Writer {
         Engine.init("http://127.0.0.1:8081", "ex_account", "123456");
         Endpoint.register(Path.CREATE_ACCOUNT_URL, new CreateAccountService());
         Command.initSingleWriter("ex_account");
-        Event.startListening();
     }
 }
