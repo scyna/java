9b7dd9cc8e24b45b24d1ceb56089e4d348c9c548
NGUYEN Hong San <hongsan@gmail.com>
2023-02-04 12:42:33 +0700

    event handler

---
+++ src/test/java/io/scyna/ex/account/build/Projection.java
@@ -0,0 +1,9 @@
+package io.scyna.ex.account.build;
+
+public class Projection {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/test/java/io/scyna/ex/account/event/AccountCreatedHandler.java
@@ -0,0 +1,13 @@
+package io.scyna.ex.account.event;
+
+import io.scyna.Event;
+import io.scyna.ex.account.proto.AccountCreated;
+
+public class AccountCreatedHandler extends Event.Handler<AccountCreated> {
+
+    @Override
+    public void execute() {
+        context.info("Receive AccountCreated event");
+        /* TODO */
+    }
+}
