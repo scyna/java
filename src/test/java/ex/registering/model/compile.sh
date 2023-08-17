791c86147c7cfd18909fa44352ebdb0e815d02a0
NGUYEN Hong San <hongsan@gmail.com>
2023-08-17 18:58:15 +0700

    registering skeleton

---
+++ src/test/java/ex/registering/model/compile.sh
@@ -0,0 +1,2 @@
+protoc -I=. --java_out=../../../ registering.proto
+protoc -I=. --java_out=../../../ adapter-send-email.proto
---
+++ src/test/java/ex/registering/services/SendOtp.java
@@ -0,0 +1,5 @@
+package ex.registering.services;
+
+public class SendOtp {
+
+}
