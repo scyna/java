2a83564ab70683b9521cda38ff32d31ff694ee32
NGUYEN Hong San <hongsan@gmail.com>
2023-08-17 18:11:09 +0700

    minor update

--- src/test/java/scyna/ex/hello/services/AddService.java
+++ src/test/java/scyna/ex/hello/services/AddService.java
@@ -8,12 +8,12 @@ public class AddService extends Endpoint.Handler<AddRequest> {
     @Override
     public void execute() throws scyna.Error {
         context.info("Receive AddRequest");
-        var sum = request.getA() + request.getB();
 
+        var sum = request.getA() + request.getB();
         if (sum > 100) {
             throw scyna.Error.REQUEST_INVALID;
-        } else {
-            response(AddResponse.newBuilder().setSum(sum).build());
         }
+
+        response(AddResponse.newBuilder().setSum(sum).build());
     }
 }
