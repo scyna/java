23c43e109d3dc4a19c5e037c9e35bc471b3fbd34
NGUYEN Hong San <hongsan@gmail.com>
2023-09-13 21:25:11 +0700

    domain event test worked

--- src/test/java/ex/registering/test/TestRegistrationCreated.java
+++ src/test/java/ex/registering/test/TestRegistrationCreated.java
@@ -3,22 +3,16 @@ package ex.registering.test;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
-
-import ex.registering.proto.CreateRegistrationRequest;
 import ex.registering.proto.RegistrationCreated;
-import ex.registering.services.CreateRegistrationHandler;
-import ex.registering.shared.Path;
 import scyna.DomainEvent;
-import scyna.Endpoint;
 import scyna.Engine;
-import scyna.Error;
 import scyna.testing.Testing;
 
-public class TestCreateRegistration {
+public class TestRegistrationCreated {
+
     @BeforeClass
     public static void setup() throws Exception {
         Engine.Init("http://127.0.0.1:8081", "scyna_test", "123456");
-        Endpoint.Register(Path.CREATE_REGISTRATION, new CreateRegistrationHandler());
     }
 
     @AfterClass
@@ -28,15 +22,28 @@ public class TestCreateRegistration {
 
     @Test
     public void testGivenGoodRequest_ShouldSuccess() throws InterruptedException {
-        Testing.Endpoint(Path.CREATE_REGISTRATION)
-                .withRequest(CreateRegistrationRequest.newBuilder()
+        Testing.DomainEvent(new RegistrationCreatedHandler())
+                .withData(RegistrationCreated.newBuilder()
                         .setEmail("a@gmail.com")
                         .setName("Nguyen Van A")
                         .setPassword("123456").build())
                 .expectDomainEvent(RegistrationCreated.newBuilder()
                         .setEmail("a@gmail.com")
-                        .setName("Nguyen Van A")
+                        .setName("Nguyen Van Axxx")
                         .setPassword("123456").build())
-                .expectSuccess().run();
+                .run();
+    }
+
+    public class RegistrationCreatedHandler extends DomainEvent.Handler<RegistrationCreated> {
+
+        @Override
+        public void Execute() throws Error {
+            System.out.println("Receive RegistrationCreated: " + data.toString());
+            context.raiseDomainEvent(RegistrationCreated.newBuilder()
+                    .setEmail(data.getEmail())
+                    .setName(data.getName() + "xxx")
+                    .setPassword(data.getPassword())
+                    .build());
+        }
     }
 }
