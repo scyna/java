e3856ef58aa512ea79d1cc43dc1ca0c1b693f1f8
NGUYEN Hong San <hongsan@gmail.com>
2023-09-14 10:28:10 +0700

    test db passed

--- src/test/java/scyna/test/TestDB.java
+++ src/test/java/scyna/test/TestDB.java
@@ -1,5 +1,7 @@
 package scyna.test;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -11,6 +13,7 @@ public class TestDB {
     @BeforeClass
     public static void setup() throws Exception {
         Engine.Init("http://127.0.0.1:8081", "scyna_test", "123456");
+        Engine.DB().execute("TRUNCATE scyna_test.test_table");
     }
 
     @AfterClass
@@ -24,5 +27,11 @@ public class TestDB {
         Engine.DB().execute("INSERT INTO scyna_test.test_table(key, value) VALUES ('key2', 'value2')");
         Engine.DB().assureExists("SELECT * FROM scyna_test.test_table WHERE key = 'key1'");
         Engine.DB().assureNotExists("SELECT * FROM scyna_test.test_table WHERE key = 'key3'");
+        var rs = Engine.DB().queryMany("SELECT * FROM scyna_test.test_table").all();
+        assertEquals(2, rs.size());
+        assertEquals("key1", rs.get(0).getString("key"));
+        assertEquals("value1", rs.get(0).getString("value"));
+        assertEquals("key2", rs.get(1).getString("key"));
+        assertEquals("value2", rs.get(1).getString("value"));
     }
 }
